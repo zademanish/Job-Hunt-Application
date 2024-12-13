cf116b7b2b44b4570ec7dba5762027f787f5e616
zademanish <manishzademz7@gmail.com>
2024-12-13 15:19:04 +0530

    cors error solve

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOption= {
-    origin:'http://localhost:5173',
+    origin:'https://job-hunt-five-delta.vercel.app/',
     credentials:true
 }
 app.use(cors(corsOption));
