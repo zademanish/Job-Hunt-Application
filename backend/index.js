69d2990421a6b4d784a647d8bd40994c4732a31a
zademanish <manishzademz7@gmail.com>
2024-12-17 17:06:11 +0530

    env change

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOption= {
-    origin:'http://localhost:5173',
+    origin:process.env.FRONT_END_URL,
     credentials:true
 }
 app.use(cors(corsOption));
